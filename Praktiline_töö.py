5466821f8a10261b225722c839d7dd14a74989c5
opilane <opilane@HP-CZC6297B9B>
2022-12-14 11:36:03 +0200

    воспользовался функциями if,try,exept...

--- Praktiline_töö.py
+++ Praktiline_töö.py
@@ -1,42 +1,60 @@
+from ast import excepthandler
 from importlib.resources import Package
 from math import *
 from stringprep import c22_specials, c6_set
 from tkinter import Pack
 from tkinter.tix import InputOnly # * -kõik funktsioonid moodulist
 from random import *
+from xml.dom.minidom import Identified
 #import math       math.funktsioon()
 #12/12/22
 #1.
 print("Puu läbimõõdu arvutamine")
 #Kirjuta programm,mis küsib kasutaja käest puu ümbermõõdu ning teatab selle peale puu läbimõõdu.
-C=float(input("Anna ümbermõõt: "))
-d=round(C/pi,2)
-print(f"Puu läbimõõt = {d}")
+try:
+    C=float(input("Anna ümbermõõt: "))
+    if C>0:
+        d=round(C/pi,2)
+        print(f"Puu läbimõõt = {d}")
+    else:
+          print("C peab olema suurem kui 0")
+except:
+    print("Andmetüüp on vale")
 
 
 
 print("Ristkülikukujulise maatüki diagonaal pikk")
-N=float(input("Siseta N: "))
-M=float(input("Siseta M: "))
-d=round(sqrt(N**2+M**2),2)
-print("Ristkülikukujulise maatüki diagonaal pikk ",round(d,2))
+try:
+    N=float(input("Siseta N: "))
+    M=float(input("Siseta M: "))
+    if M>0 and N>0:
+        d=round(sqrt(N**2+M**2),2)
+        print("Ristkülikukujulise maatüki diagonaal pikk ",round(d,2))
+except:
+    print("N jf M olema suurem kui 0")
 
 
-aeg = float(input("Mitu tundi kulus sõiduks? "))
-teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
-kiirus = teepikkus/aeg
-print("Sinu kiirus oli " + str(kiirus) + " km/h")
+try:
+    aeg = float(input("Mitu tundi kulus sõiduks? "))
+    teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
+    if aeg>0 and teepikkus>0:
+        kiirus = teepikkus / aeg
+        print("Sinu kiirus oli " + str(kiirus) + " km/h")
 
-
+except:
+    print("M ja N vaja sisetafda float formaadis")
 
 print("Aritmeetiline keskmine")
 A1=int(input("Esimine arv: "))
 A2=int(input("Teine arv:   "))
 A3=int(input("Kolmas arv:  "))
 A4=int(input("Neljas arv:   "))
-A5=int(input("Viies arv:   "))
-K=(A1+A2+A3+A4+A5)/5
-print(f"Kesknime on {K}")
+A5=int(input("Viies arv:   if"))
+if A1>0 and A2>0 and A3>0 and A4>0 and A5>0:
+    K=(A1+A2+A3+A4+A5)/5
+else:
+    print("aeg ja teepikkus vaja sisetada float formaadis ")
+    print(f"Kesknime on {k}")
 
 
 
@@ -88,4 +106,23 @@ H=M//60 #h
 M=M%60 #min
 print(f"{H}:{M}")
 
+
+
+
+
+print("Ema mis hined sa koolis said?")
+a=input("Siseta: ")
+print(a.isalpha(), a.isdigit())
+if a.isdigit() and int(a)>0 and int(a)<=5:
+    a=int(a)
+    if a==5:
+        pass
+    elif a==4:
+        pass
+    elif a==3:
+        pass
+    elif a==2 or a==1:
+            pass
+else:
+    print("Sa valesti vastas")
 
